ccb215083dfdc2a3ecd059e42b6812a20145b8eb
ksang <ksang@umich.edu>
2020-12-31 18:01:08 -0500

    define function headers

--- bplustree.h
+++ bplustree.h
@@ -1,20 +1,46 @@
+#ifndef _BPLUSTREE_H_
+
 #include <iostream>
 #include <string>
+#include <vector>
+#include "params.h"
 
-struct Node {
-    Node* parent;
+struct BNode {
+    unsigned char data[PAGE_SIZE_IN_BYTES];
 };
 
-struct TreeNode : Node {
-    
-};
-
-struct LeafNode : Node {
-
-};
-
+template <class KeyT, class DataT>
 class BPlusTree {
 public:
+    BPlusTree();
 
+    void insertKeyVal(KeyT key, DataT &data) {
+
+    }
+
+    void deleteKeyVal(KeyT key) {
+
+    }
+
+    DataT findKeyVal(KeyT key) {
+
+    }
+
+    KeyT findKey(KeyT key) {
+        
+    }
+
+    std::vector<DataT> findLessThan(KeyT key) {
+
+    }
+
+    std::vector<DataT> findGreaterThan(KeyT key) {
+
+    }
+
+    ~BPlusTree();
 private:
+    BNode* root;
 };
+
+#endif
---
+++ params.h
@@ -0,0 +1,8 @@
+#ifndef _PARAMS_H_
+
+/*
+ * Number of bytes in a page
+ */
+static const unsigned int PAGE_SIZE_IN_BYTES = 4096;
+
+#endif

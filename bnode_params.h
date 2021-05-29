06789790be417090c93b4076b9cd9236ebc4fec8
ksang <ksang@umich.edu>
2021-05-29 18:17:19 -0400

    removed parent pointer index variable from bnode_params.h

--- bnode_params.h
+++ bnode_params.h
@@ -8,14 +8,9 @@
 static const size_t ELEMENT_COUNT_INDEX = 0;
 
 /*
- * Starting index of a BNode's parent pointer
- */
-static const size_t PARENT_PTR_INDEX = ELEMENT_COUNT_INDEX + SIZE_T_SIZE_IN_BYTES;
-
-/*
  * Starting index of left sibling pointer for a LeafBNode
  */
-static const size_t LEFT_SIBLING_PTR_INDEX = SIZE_T_SIZE_IN_BYTES + POINTER_SIZE_IN_BYTES;
+static const size_t LEFT_SIBLING_PTR_INDEX = ELEMENT_COUNT_INDEX + SIZE_T_SIZE_IN_BYTES;
 
 /*
  * Starting index of right sibling pointer for a LeafBNode
@@ -30,6 +25,6 @@ static const size_t LEAF_FIRST_ELEMENT_INDEX = RIGHT_SIBLING_PTR_INDEX + POINTER
 /*
  * Starting index of the first element of a TreeBNode
  */
-static const size_t TREE_FIRST_ELEMENT_INDEX = PARENT_PTR_INDEX + POINTER_SIZE_IN_BYTES;
+static const size_t TREE_FIRST_ELEMENT_INDEX = ELEMENT_COUNT_INDEX + SIZE_T_SIZE_IN_BYTES;
 
 #endif
